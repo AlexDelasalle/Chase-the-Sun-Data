a26783f943040aea7da919c5479d0abd4a44ce1c
AlexDelasalle <alex_delasalle@aol.co.uk>
2023-06-21 14:01:49 +0100

    Fully funtional app (still needs designing)

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,39 @@
-// --------------CURRENT TIME----------------
+let speed;
+let distance;
+let startTime;
+let breakTime;
+let targetETA;
+
+
+function storeSpeed() {
+  const speedInput = document.getElementById("speedInput").value;
+  speed = speedInput;
+  // You can manipulate and change the "speed" variable as needed here
+  document.getElementById("speedDisplay").textContent = speed + "mph";
+}
+
+function storeDistance() {
+  const distanceInput = document.getElementById("distanceInput").value;
+  distance = distanceInput;
+  // You can manipulate and change the "distance" variable as needed here
+  document.getElementById("distanceDisplay").textContent = distance + "miles";
+}
+
+function storeStartTime() {
+  const startTimeInput = document.getElementById("startTimeInput").value;
+  startTime = startTimeInput;
+  // You can manipulate and change the "startTime" variable as needed here
+  document.getElementById("startTimeDisplay").textContent = startTime;
+}
+
+function storeBreakTime() {
+  const breakTimeInput = document.getElementById("breakTimeInput").value;
+  breakTime = breakTimeInput;
+  // You can manipulate and change the "startTime" variable as needed here
+  document.getElementById("breakTimeDisplay").textContent = breakTime;
+}
+
+//--------------CURRENT TIME----------------
 function updateTime() {
   var now = new Date();
   var timeSpan = document.getElementById("current-time");
@@ -6,51 +41,194 @@ function updateTime() {
 }
 setInterval(updateTime, 1000); // update every second
 
-//--------------HIDING THE INPUT BOX AND SUBMIT BUTTON------------
-function handleClick() {
-  const inputBox = document.getElementById("input-box");
-  const displayText = document.getElementById("display-text");
-  const enterButton = document.getElementById("enter-button");
-  const inputValue = inputBox.value;
+// ------ Distance Remaining ------
+function storeDistance() {
+  var distanceInput = document.getElementById("distanceInput");
+  var remainingDistance = 205 - distanceInput.value;
+  document.getElementById("remainingDistanceDisplay").textContent = remainingDistance;
+}
 
-  displayText.innerText = inputValue;
-  displayText.style.display = "block";
-  inputBox.style.display = "none";
-  enterButton.style.display = "none";
+// Add an event listener to the distance input field
+var distanceInput = document.getElementById("distanceInput");
+distanceInput.addEventListener("input", storeDistance);
+
+
+// ----------TIME TO FINISH -------------
+
+function calculateTimeToFinish() {
+  const speedInput = parseFloat(document.getElementById("speedInput").value);
+  const distanceRemaining = parseFloat(document.getElementById("remainingDistanceDisplay").textContent);
+  const breakTimeRemaining = parseFloat(document.getElementById("breakTimeInput").value);
+
+  const timeToFinish = (distanceRemaining / speedInput) * 60; // Time in minutes
+  const totalTime = timeToFinish + breakTimeRemaining;
+
+  const hours = Math.floor(totalTime / 60);
+  const minutes = Math.floor(totalTime % 60);
+
+  const timeToFinishDisplay = document.getElementById("timeToFinishDisplay");
+  timeToFinishDisplay.textContent = hours + "h " + minutes + "m";
+}
+
+// Add event listeners
+document.getElementById("speedInput").addEventListener("input", calculateTimeToFinish);
+document.getElementById("remainingDistanceDisplay").addEventListener("input", calculateTimeToFinish);
+document.getElementById("breakTimeInput").addEventListener("input", calculateTimeToFinish);
+document.getElementById("startTimeInput").addEventListener("input", calculateTimeToFinish);
+document.getElementById("distanceInput").addEventListener("input", calculateTimeToFinish);
+
+//------ Calculating current ETA ------
+
+setInterval(displayCurrentTimePlusTimeToFinish, 1000);
+
+function displayCurrentTimePlusTimeToFinish() {
+  const currentTimeElement = document.getElementById("current-time");
+  const timeToFinishElement = document.getElementById("timeToFinishDisplay");
+
+  const currentTimeText = currentTimeElement.innerHTML;
+  const timeToFinishText = timeToFinishElement.textContent;
+
+  // Extract hours and minutes from timeToFinishText
+  const regex = /(\d+)h (\d+)m/; // Updated regex pattern to match the format "Xh Ym"
+  const match = timeToFinishText.match(regex);
+
+  if (match) {
+    const hours = parseInt(match[1]);
+    const minutes = parseInt(match[2]);
+
+    // Create a new Date object and add hours and minutes to it
+    const currentTime = new Date();
+    currentTime.setHours(currentTime.getHours() + hours);
+    currentTime.setMinutes(currentTime.getMinutes() + minutes);
+
+    // Format the result as a string
+    const options = { hour: 'numeric', minute: 'numeric' };
+    const currentTimePlusTimeToFinish = currentTime.toLocaleTimeString(undefined, options);
+
+    // Display the result
+    document.getElementById("finishTimeDisplay").textContent = currentTimePlusTimeToFinish;
+  } else {
+    document.getElementById("finishTimeDisplay").textContent = "N/A";
+  }
+}
+
+// -------- Calculating extra break time allowed -----
+setInterval(calculateTimeDifference, 1000);
+
+function calculateTimeDifference() {
+  const currentETADisplay = document.getElementById("finishTimeDisplay");
+  const currentETA = currentETADisplay.textContent;
+
+  // Validate current ETA format
+  const currentETAMatch = currentETA.match(/^(\d{1,2}):(\d{2})$/);
+  if (!currentETAMatch) {
+    const extraBreakTimeDisplay = document.getElementById("extraBreakTimeDisplay");
+    extraBreakTimeDisplay.textContent = "N/A";
+    return;
+  }
+
+  // Extract hours and minutes from current ETA
+  const currentHours = parseInt(currentETAMatch[1]);
+  const currentMinutes = parseInt(currentETAMatch[2]);
+
+  // Convert current ETA to minutes
+  const currentETAInMinutes = currentHours * 60 + currentMinutes;
+
+  // Convert target ETA ("21:00") to minutes
+  const targetETAInMinutes = 21 * 60;
+
+  // Calculate the time difference in minutes
+  const timeDifferenceInMinutes = targetETAInMinutes - currentETAInMinutes;
+
+  // Convert the time difference to hours and minutes
+  const timeDifferenceHours = Math.floor(timeDifferenceInMinutes / 60);
+  const timeDifferenceMinutes = timeDifferenceInMinutes % 60;
+
+  // Update the content of the extraBreakTimeDisplay h2 element with the time difference
+  const extraBreakTimeDisplay = document.getElementById("extraBreakTimeDisplay");
+  extraBreakTimeDisplay.textContent = timeDifferenceHours + "h " + timeDifferenceMinutes + "m";
+}
+
+// ------------ Calculating time amassed --------------------
+
+function calculateTimeAmassed() {
+  const currentTime = new Date();
+  const startTimeInput = document.getElementById("startTimeInput").value;
+  const [startHours, startMinutes] = startTimeInput.split(":");
+  const startTime = new Date();
+  startTime.setHours(startHours, startMinutes, 0, 0);
+
+  // Calculate the time difference in milliseconds
+  const timeDifference = currentTime - startTime;
+
+  // Convert the time difference to hours and minutes
+  const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+
+  // Display the result
+  const timeAmassedDisplay = document.getElementById("timeAmassedDisplay");
+  timeAmassedDisplay.textContent = hours + " hrs " + minutes + " mins";
+}
+
+// Add an event listener to the startTimeInput field
+var startTimeInput = document.getElementById("startTimeInput");
+startTimeInput.addEventListener("input", calculateTimeAmassed);
+
+
+// -------------Sunrise-Sunset API-------------------------
+
+const latitude1 = 51.4168969; // Example latitude for Location 1
+const longitude1 = 0.808862; // Example longitude for Location 1
+const latitude2 = 51.3471927; // Example latitude for Location 2
+const longitude2 = -2.9778916; // Example longitude for Location 2
+
+const apiUrl1 = `https://api.sunrise-sunset.org/json?lat=${latitude1}&lng=${longitude1}`;
+const apiUrl2 = `https://api.sunrise-sunset.org/json?lat=${latitude2}&lng=${longitude2}`;
+
+// --- SUNRISE ---
+fetch(apiUrl1)
+  .then(response => response.json())
+  .then(data => {
+    const sunriseTime = parseTimeString(data.results.sunrise);
+
+    const sunriseElement = document.getElementById('sunrise');
+    sunriseElement.textContent = `Sunrise: ${sunriseTime}`;
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+// --- SUNSET ---
+fetch(apiUrl2)
+  .then(response => response.json())
+  .then(data => {
+    const sunsetTime = parseTimeString(data.results.sunset);
+
+    const sunsetElement = document.getElementById('sunset');
+    sunsetElement.textContent = `Sunset: ${sunsetTime}`;
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+function parseTimeString(timeString, use24HourFormat) {
+  const time = timeString.split(' ')[0]; // Extract time part without AM/PM
+  const [hours, minutes, seconds] = time.split(':');
+  const date = new Date();
+  if (use24HourFormat) {
+    date.setUTCHours(hours, minutes, seconds);
+    return date.toISOString().substr(11, 5); // Extract hours and minutes in 24-hour format
+  } else {
+    date.setUTCHours(hours % 12, minutes, seconds);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  }
 }
 
 //  ------------FINDING THE TIME UNTIL SUNSET--------------
 
-function updateTimeDiff() {
-  // Get the current time
-  const now = new Date();
 
-  // Define the sunset time (replace with your own variable)
-  const sunsetTime = new Date("2023-06-24T21:23:00");
-
-  // Calculate the difference between the current time and sunset time in milliseconds
-  const diffInMillis = sunsetTime - now;
-
-  // Convert the difference to hours, minutes, and seconds
-  const diffInHours = Math.floor(diffInMillis / (1000 * 60 * 60));
-  const diffInMinutes = Math.floor((diffInMillis % (1000 * 60 * 60)) / (1000 * 60));
-  const diffInSeconds = Math.floor((diffInMillis % (1000 * 60)) / 1000);
-
-  // Format the time difference as "h:mm:ss"
-  const formattedTimeDiff = `${diffInHours}:${diffInMinutes.toString().padStart(2, "0")}:${diffInSeconds.toString().padStart(2, "0")}`;
-
-  // Get the h2 tag by its ID
-  const timeDiffTag = document.getElementById("time-diff");
-
-  // Set the innerHTML of the h2 tag to display the formatted time difference
-  timeDiffTag.innerHTML = `${formattedTimeDiff}`;
-}
-
-// Call updateTimeDiff() immediately to update the time difference when the page loads
-updateTimeDiff();
-
-// Call updateTimeDiff() every second to update the time difference continuously
-setInterval(updateTimeDiff, 1000);
 
 //----------MAN ON BIKE-----------
 const biker = document.getElementById('biker');
